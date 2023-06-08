cbee7385cc013b16d15d1bb29788ddf3ec367b2e
Md annoor Sharif <128777957+ontugt@users.noreply.github.com>
2023-06-08 11:19:12 +0600

    Update middleware.js

---
+++ middleware.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+function authenticate(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    req.userId = decoded.userId;
+    next();
+  });
+}
