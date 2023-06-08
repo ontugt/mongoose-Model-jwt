508173fa2e0184aad84adb173583f2ee86d65cf0
Md annoor Sharif <128777957+ontugt@users.noreply.github.com>
2023-06-08 11:17:32 +0600

    Update product.js

---
+++ models/product.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+// Define the schema
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Create the model
+const Product = mongoose.model('Product', productSchema);
+
+// Export the model
+module.exports = Product;
